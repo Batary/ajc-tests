619b3721dfee30e5ffcee64c0f3a7e1bc874b576
Kévin Da Silva <kevin.dasilva3@outlook.fr>
2022-12-21 16:22:54 +0100

    Ajout des propriétés taille et poids dans la classe src\test\ajc\Humain.java

--- src/ajc/test/Human.java
+++ src/ajc/test/Human.java
@@ -3,13 +3,16 @@ package ajc.test;
 public abstract class Human {
 
 	String name;
+	int taille;
+	int poids;
 	
-	Human(String name) {
+	Human(String name, int taille, int poids) {
 		this.name = name;
-		
+		this.taille = taille;
+		this.poids = poids;
 	}
 	
 	public void print() {
-		System.out.println(name);
+		System.out.println("Nom: " + name + "Taille: " + taille + "Poids: " + poids);
 	}
 }
