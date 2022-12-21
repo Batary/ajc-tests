4577f7e22b71452b8a1c3eea16ce9fc10abbbe02
anastasia toullec <chychko.n@gmail.com>
2022-12-21 15:25:24 +0100

    Anastasia commit

---
+++ HelloWorld.java
@@ -0,0 +1,11 @@
+package chapitre1;
+
+public class HelloWorld {
+	/* un exemple pour ecrire plusieures lignes 
+	 * de commentaires*/
+	public static void main (String [] args) {
+		//un commentaire pour une seule ligne
+		System.out.println("Bonjour tt le monde");
+	}
+
+}
