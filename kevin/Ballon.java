b56ceed33df2d41156b5d6a4d402ec5e02f27db0
Kévin Da Silva <kevin.dasilva3@outlook.fr>
2022-12-21 15:52:19 +0100

    Ajout de la classe Ballon et création d'un ballon dans le main

---
+++ kevin/Ballon.java
@@ -0,0 +1,13 @@
+package kevin;
+
+public class Ballon{
+	public int diametre;
+	public String couleur;
+	public String type;
+
+	public Ballon(int d, String c, String t){
+		this.diametre = d;
+		this.couleur = c;
+		this.type = t;
+	}
+}
